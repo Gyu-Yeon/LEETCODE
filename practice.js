58c0918322eb13bc930fee508dc07e2f5dd4d01c
Johnny Choi <79036459+Gyu-Yeon@users.noreply.github.com>
2021-03-18 14:52:36 +0900

    Add files via upload

--- practice.js
+++ practice.js
@@ -52,3 +52,55 @@ let countMatches = function(items, ruleKey, ruleValue) {
         }
     }return count;     
 }
+
+//Q4 
+
+let smallerNumbersThanCurrent = function(nums) {    
+    let arr = [];
+    for(let i = 0; i<nums.length; i++){
+        let sum = 0;
+        for(let j = 0; j<nums.length; j++){
+            if(nums[i] > nums[j]){
+                sum = sum +1;
+            }
+        }arr.push(sum);
+    }
+   return arr
+}
+
+//Q5 
+let countSmaller = function(nums) {
+    let arr = [];
+    for(let i = 0; i<nums.length; i++){
+        let sum = 0;
+        for(let j = i+1; j<nums.length; j++){
+            if(nums[i] > nums[j]){
+                sum = sum +1;
+            }
+        }arr.push(sum);
+    }
+   return arr
+}
+
+//Q6 
+let numberOfSteps  = function(num) {
+    let count = 0;
+    do{
+if(num == 0){
+   return count
+// step1. divide by 2 if the input num is even
+}else if(num%2 == 0){
+    num = num/2
+    count ++
+// step2. subtract 1 if the input num is odd       
+}else {
+    num = num-1
+    count++
+}
+// step3. keep going with step1 and step2 until the num gets to 0
+}while(num> 0)
+// step4. count how many process has been through
+return count;
+}
+
+
